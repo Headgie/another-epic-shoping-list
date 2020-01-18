4d1e23734cc467047f05ddaff1f69e3c78d9a3bb
Headgie <headgy@yandex.ru>
2020-01-18 23:13:58 +0300

    немного стрелочных функций и прочего ES6

--- script.js
+++ script.js
@@ -1,52 +1,48 @@
-var ul = document.querySelector("ul");
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
+const ul = document.querySelector("ul");
+const button = document.getElementById("enter");
+const input = document.getElementById("userinput");
 
-function inputLength() {
-  return input.value.length;
-}
+const inputLength = () => input.value.length;
 
-function addElementToList() {
-  var li = document.createElement("li");
-  var label = document.createElement("span");
+const addElementToList = () => {
+  let li = document.createElement("li");
+  let label = document.createElement("span");
   label.classList.add("label");
   label.appendChild(document.createTextNode(input.value));
   li.appendChild(label);
-  var bt = document.createElement("button");
-  var icon = document.createElement("i");
-  icon.classList.add("fa");
-  icon.classList.add("fa-times");
   
+  let bt = document.createElement("button");
+  let icon = document.createElement("i");
+  icon.classList.add("fa", "fa-times");
   bt.appendChild(icon);
   bt.classList.add("remove");
   bt.addEventListener("click", removeListItem);
-
   li.appendChild(bt);
+
   li.addEventListener("click", listItemClick);
   ul.appendChild(li);
   input.value = "";
-}
+};
 
-function addListAfterClick() {
+button.onclick = () => {
   if (inputLength() > 0) {
     addElementToList();
   }
-}
+};
 
-function addListAfterKeypress(e) {
+input.onkeypress = (e) => {
   if (e.keyCode === 13 && inputLength() > 0) {
     addElementToList();
   }
-}
+};
 
-function listItemClick () {
+function listItemClick() {
   this.firstElementChild.classList.toggle("done");
+  const out = () => console.log(this);
+  out();
 }
 
 function removeListItem() {
   var li = this.parentElement;
   li.parentElement.removeChild(li);
 }
-
-input.addEventListener("keypress", addListAfterKeypress);
-button.addEventListener("click", addListAfterClick);
